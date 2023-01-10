c41860bd454af84f3d30a6179086df29b5ee82eb
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2023-01-09 22:48:15 -0800

    Delete Functionality

--- lib/views/view_categories.dart
+++ lib/views/view_categories.dart
@@ -1,5 +1,7 @@
 import 'package:cashbuddy_mobile/constants/routes.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:gap/gap.dart';
 // Constants
 import 'package:cashbuddy_mobile/constants/colors.dart';
@@ -28,6 +30,72 @@ class _ViewCategoriesState extends State<ViewCategories> {
     super.initState();
   }
 
+  Future<bool> showNativeDialog(BuildContext context) async {
+    final navigator = Navigator.of(context);
+
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return await showDialog<bool>(
+            context: context,
+            barrierDismissible: false,
+            builder: ((context) {
+              return AlertDialog(
+                title: const Text('Delete Category'),
+                content: const Text(
+                    'Are you sure you want to delete this category?'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      navigator.pop(false);
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      navigator.pop(true);
+                    },
+                    child: const Text(
+                      'Confirm',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ],
+              );
+            }),
+          ) ??
+          false;
+    } else {
+      return await showCupertinoDialog<bool>(
+            context: context,
+            barrierDismissible: false,
+            builder: ((context) {
+              return CupertinoAlertDialog(
+                title: const Text('Delete Category'),
+                content: const Text(
+                    'Are you sure you want to delete this category?'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      navigator.pop(false);
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      navigator.pop(true);
+                    },
+                    child: const Text(
+                      'Confirm',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ],
+              );
+            }),
+          ) ??
+          false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final navigator = Navigator.of(context);
@@ -42,7 +110,8 @@ class _ViewCategoriesState extends State<ViewCategories> {
               padding: const EdgeInsets.all(20),
               itemCount: snapshot.data!.size,
               itemBuilder: (context, index) {
-                final title = snapshot.data!.docs[index].data()['title'] as String;
+                final title =
+                    snapshot.data!.docs[index].data()['title'] as String;
                 final id = snapshot.data!.docs[index].id;
                 return ListTile(
                   title: Text(title),
@@ -69,7 +138,14 @@ class _ViewCategoriesState extends State<ViewCategories> {
                       padding: EdgeInsets.zero,
                       constraints: const BoxConstraints(),
                       icon: const Icon(Icons.delete),
-                      onPressed: () {},
+                      onPressed: () async {
+                        final db = FirebaseFirestore.instance;
+                        final result = await showNativeDialog(context);
+
+                        if (result) {
+                          await db.collection('categories').doc(id).delete();
+                        }
+                      },
                     ),
                   ]),
                 );
